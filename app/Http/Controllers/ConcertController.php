5e34f4fcdad68ae24b3ac1b13507c2b5dae3bf0a
ArthurDOTmkv <Arthur-a379@hotmail.com>
2021-03-28 14:42:30 +0200

    Update controller concert + modification de la page du site

--- app/Http/Controllers/ConcertController.php
+++ app/Http/Controllers/ConcertController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Concert;
 use Illuminate\Http\Request;
 
 class ConcertController extends Controller
 {
     public function index()
     {
-        return view('concerts.index');
+        $concerts = Concert::latest()->take(8)->get();
+        //dd($concerts);
+        return view('concerts.index')->with('concerts', $concerts);
     }
 }
