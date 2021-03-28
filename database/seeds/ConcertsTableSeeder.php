066083059956b94412d2f0812220cd01f2129987
ArthurDOTmkv <Arthur-a379@hotmail.com>
2021-03-28 16:17:30 +0200

    Correction affichage

--- database/seeds/ConcertsTableSeeder.php
+++ database/seeds/ConcertsTableSeeder.php
@@ -18,11 +18,11 @@ class ConcertsTableSeeder extends Seeder
         for($i=0; $i<20; $i++)
         {
             Concert::create([
-               'titre' => $faker->sentence(5),
+               'titre' => $faker->sentence(6, false),
                'slug' => $faker->slug,
-               'soustitre' => $faker->sentence(10),
+               'soustitre' => $faker->sentence(15, false),
                'description' => $faker->text,
-               'prix' => $faker->numberBetween(30, 120) * 100,          //Il est de bonne pratique de stocker le prix en centimes dans la DB
+               'prix' => $faker->numberBetween(30, 120) * 100,          //Stocker le prix en centimes dans la DB (Best practice)
                'image' => 'https://via.placeholder.com/200x250'
             ]);
         }
