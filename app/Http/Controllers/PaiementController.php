5e18313f64528b506bf0ce19d7404f1026ae7b1c
ArthurDOTmkv <Arthur-a379@hotmail.com>
2021-04-01 19:53:18 +0200

    Update PaiementController

--- app/Http/Controllers/PaiementController.php
+++ app/Http/Controllers/PaiementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Stripe\Stripe;
 use Illuminate\Http\Request;
 
 class PaiementController extends Controller
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,8 +10,9 @@
     <title>Blog Template · Bootstrap v5.0</title>
 
     <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/blog/"> -->
-
     
+    <!-- Inclus la détection de fraude -->
+    <script src="https://js.stripe.com/v3/"></script>
 
     <!-- Bootstrap core CSS -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
