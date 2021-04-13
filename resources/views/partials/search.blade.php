7cbf99b43b46b4021db21e1f09bf11b113ca8bbb
ArthurDOTmkv <Arthur-a379@hotmail.com>
2021-04-13 11:09:13 +0200

    Formulaire de rechererche

---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,6 @@
+<form class="d-flex" action="{{route('concerts.search')}}">
+    <div class="form-group">
+        <input class="form-control" type="text" name="search">
+    </div>
+    <button class="btn btn-secondary" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+</form>
