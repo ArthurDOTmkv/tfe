f945acc4eb4f6595a10963992d31aa71859f1769
ArthurDOTmkv <Arthur-a379@hotmail.com>
2021-04-18 14:33:53 +0200

    Update historique des commandes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,10 +17,16 @@
                     @foreach(Auth()->user()->commandes as $commande)
                     <div class="card">
                         <div class="card-header">
-                            Commande du {{Carbon\Carbon::parse($commande->paymentCreatedAt)->format('d/m/Y')}} <strong>({{getPrix($commande->montant)}})</strong>
+                            Commande du {{Carbon\Carbon::parse($commande->paymentCreatedAt)->format('d/m/Y')}} <strong>({{getPrix($commande->montant)}} TVA inclus)</strong>
                         </div>
                         <div class="card-body">
-                            
+                            <h5><strong>Liste des concerts</strong></h5>
+                            @foreach(unserialize($commande->concerts) as $concert)
+                            <div><b>Concert :</b> {{$concert[0]}}</div>
+                            <div><b>Prix :</b> {{getPrix($concert[1])}}</div>
+                            <div><b>Nombre de places :</b> {{$concert[2]}}</div>    
+                            </br>
+                            @endforeach
                         </div>
                     </div>
                     @endforeach
